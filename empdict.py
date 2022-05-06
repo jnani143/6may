9f4862c08678695dc93679a336182f73133b38a3
jnani143 <99253160+jnani143@users.noreply.github.com>
2022-05-06 18:12:22 +0530

    Create empdict.py

---
+++ empdict.py
@@ -0,0 +1,13 @@
+emp={1:['gopi','manager','17-01-2009',50000],2:['raju','assmanager','18-01-2010',30000],3:['hari','sales','19-12-2014',25000],4:['rani','sales','26-09-2012',15000],5:['ravi','sales','30-12-2017',10000]}
+x=int(input('enter empno:'))
+print('empname:',emp[x][0])
+print('empdesign:',emp[x][1])
+print('date of joining:',emp[x][2])
+print('salary:',emp[x][3])
+
+output:
+enter empno:1
+empname: gopi
+empdesign: manager
+date of joining: 17-01-2009
+salary: 50000
